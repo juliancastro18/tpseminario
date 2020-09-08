dfdd5ef4c446635c804d7651f46a65c68ade1d1b
Julian Castro <juliancastro18k@gmail.com>
2020-09-08 03:08:11 -0300

    Octavo commit :D

--- main.py
+++ main.py
@@ -10,16 +10,17 @@ def main():
     
     pygame.init()
     enJuego = True
-    rapidroll = MiniJuegos.RapidRoll.RapidRoll((configuration.SCREEN_WIDTH-50, 40))
-    #snake = MiniJuegos.Snake.Game()
     
     while enJuego:
 
+        # el pong tendria que tener un metodo para obtener la pos de la bola cuando terminó de ejecutar
+        # y tambien otro que devuelva una lista con las barras del pong
         rapidroll = MiniJuegos.RapidRoll.RapidRoll((configuration.SCREEN_WIDTH-50, 40))
         while rapidroll.get_game_state()['alive'] and rapidroll.get_game_state()['playing']:
             rapidroll.process()
             rapidroll.display_frame()
 
+        #snake = MiniJuegos.Snake.Game()
         #while not snake.get_game_state()["done"] and snake.get_game_state()["snake_is_alive"]:
         #    snake.process()
         #    snake.display_frame()
