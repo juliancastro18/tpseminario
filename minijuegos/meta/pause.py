a020a969058b555a53fe0b3de02d7fc846bc3d4b
Julian Castro <juliancastro18k@gmail.com>
2020-09-14 19:13:47 -0300

    Corrige sonidos

--- minijuegos/meta/pause.py
+++ minijuegos/meta/pause.py
@@ -6,8 +6,11 @@ class Pause():
 	def __init__(self):
 		self._font = pygame.font.Font('data\\font\\dpcomic.ttf', 80)
 		self._background = pygame.image.load("data\\img\\grid.png")
-		
+		self._sound_pause = pygame.mixer.Sound('data\\sound\\pause.wav')
+		self._sound_pause.set_volume(0.5)
+
 		self._texto = "PAUSA"
+		self._mostrandoTexto = False
 		self._displayText = True
 		self._timer = 0
 
@@ -17,6 +20,11 @@ class Pause():
 		screen.blit(self._background, (0,0))
 
 		if self._displayText:
+
+			if self._mostrandoTexto == False:
+				self._sound_pause.play()
+				self._mostrandoTexto = True
+
 			text_Obj = self._font.render(self._texto,0,color.BLACK,screen)
 			text_rect = text_Obj.get_rect()
 			text_rect.center = ((configuration.SCREEN_WIDTH/2)+5, (configuration.SCREEN_HEIGHT/2)+5)
@@ -26,6 +34,8 @@ class Pause():
 			text_rect = text_Obj.get_rect()
 			text_rect.center = (configuration.SCREEN_WIDTH/2, configuration.SCREEN_HEIGHT/2)
 			screen.blit(text_Obj, text_rect)
+		else:
+			self._mostrandoTexto = False
 
 		if self._timer > 20:
 			self._timer = 0
